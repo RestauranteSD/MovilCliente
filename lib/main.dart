3f5a60934b8e474e666ad040a6f2b2a95c8bb7d4
Laura Nuñez <njm0031533@est.univalle.edu>
2025-02-18 00:43:07 -0400

    Diseño mejorado

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +14,90 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: QRScannerScreen(),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => WelcomeScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SpinKitRotatingCircle(
+              color: Colors.orange,
+              size: 100.0,
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Bienvenido a Hugo's Restaurant",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.qr_code_scanner, size: 100, color: Colors.orange),
+            SizedBox(height: 20),
+            Text(
+              "Escanea el código QR para ver nuestros menús",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 30),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.orange,
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => QRScannerScreen()),
+                );
+              },
+              child: Text(
+                "Escanear QR",
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -26,13 +111,12 @@ class _QRScannerScreenState extends State<QRScannerScreen> {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   QRViewController? controller;
   String menuInfo = "";
-  bool scanCompleted = false; // Controla la visibilidad del botón
+  bool scanCompleted = false;
 
   void onQRViewCreated(QRViewController controller) {
     this.controller = controller;
     controller.scannedDataStream.listen((scanData) async {
-      controller.pauseCamera(); // Pausar la cámara después de escanear
-
+      controller.pauseCamera();
       String scannedUrl = scanData.code ?? "";
       if (scannedUrl.isNotEmpty) {
         await fetchMenuData(scannedUrl);
@@ -44,20 +128,26 @@ class _QRScannerScreenState extends State<QRScannerScreen> {
     try {
       Uri uri = Uri.parse(url);
       final response = await http.get(uri);
-
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
         setState(() {
-          menuInfo = "Menú: ${data['nombre']}\nPlatos:\n" +
+          menuInfo = "${data['nombre']}\n\nPlatos:\n" +
               (data['platos'] as List)
-                  .map((plato) => "- ${plato['nombre']} (\$${plato['precio']})\n")
+                  .map((plato) {
+                String nombre = plato['nombre'];
+                String descripcion = plato['descripcion'];
+                double precio = double.tryParse(plato['precio'].toString()) ?? 0.0;  // Aseguramos que el precio sea un double
+
+                return "- $nombre ...................... Bs.${precio.toStringAsFixed(2)}\n" +
+                    "  *${descripcion}*\n\n";
+              })
                   .join("");
-          scanCompleted = true; // Mostrar el botón después del escaneo
+          scanCompleted = true;
         });
       } else {
         setState(() {
           menuInfo = "Error al obtener los datos del menú";
-          scanCompleted = true; // Aún mostrar el botón si hubo error
+          scanCompleted = true;
         });
       }
     } catch (e) {
@@ -70,10 +160,10 @@ class _QRScannerScreenState extends State<QRScannerScreen> {
 
   void restartScan() {
     setState(() {
-      menuInfo = ""; // Borrar información del menú
-      scanCompleted = false; // Ocultar el botón
+      menuInfo = "";
+      scanCompleted = false;
     });
-    controller?.resumeCamera(); // Reactivar la cámara para escanear otro QR
+    controller?.resumeCamera();
   }
 
   @override
@@ -85,29 +175,75 @@ class _QRScannerScreenState extends State<QRScannerScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Escáner QR")),
+      appBar: AppBar(
+        title: Text("Escáner QR"),
+        backgroundColor: Colors.orange,
+        elevation: 0,
+      ),
       body: Column(
         children: [
-          Expanded(
-            flex: 2,
-            child: QRView(
-              key: qrKey,
-              onQRViewCreated: onQRViewCreated,
+          if (!scanCompleted) // Solo mostrar el escáner si no se completó el escaneo
+            Expanded(
+              flex: 2,
+              child: Container(
+                margin: EdgeInsets.symmetric(vertical: 20),
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.orange[50],
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 10,
+                      offset: Offset(0, 4),
+                    ),
+                  ],
+                ),
+                child: Center(
+                  child: QRView(
+                    key: qrKey,
+                    onQRViewCreated: onQRViewCreated,
+                  ),
+                ),
+              ),
             ),
-          ),
           Expanded(
             flex: 1,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(menuInfo, textAlign: TextAlign.center),
-                SizedBox(height: 20),
-                if (scanCompleted) // Mostrar botón solo después del escaneo
-                  ElevatedButton(
-                    onPressed: restartScan,
-                    child: Text("Volver a escanear"),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Estilizamos la información del menú
+                  Text(
+                    menuInfo,
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black87,
+                      height: 1.6,  // Alineamos el texto con más espacio
+                    ),
                   ),
-              ],
+                  SizedBox(height: 20),
+                  if (scanCompleted)
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.orange,
+                        padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      onPressed: restartScan,
+                      child: Text(
+                        "Volver a escanear",
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                    ),
+                ],
+              ),
             ),
           ),
         ],
@@ -115,3 +251,5 @@ class _QRScannerScreenState extends State<QRScannerScreen> {
     );
   }
 }
+
+
